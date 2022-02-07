e203b98e9c0bffdc5ebaa0412c626fb24447c52d
AbdSh <ashmaitelly@gmail.com>
2022-02-07 12:18:23 +0200

    Added Navbar

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+
+import {NavMenu} from './Components/NavMenu';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <div className="container">
+     <h3 className="m-3 d-flex justify-content-center">
+       Grappa Systems Recruitment Exercise
+     </h3>
+
+     <NavMenu/>
     </div>
+    </BrowserRouter>
   );
 }
 
