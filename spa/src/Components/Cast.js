a150bbbc39428b2b1902fbbf3c827de4bdb0f9e7
AbdSh <ashmaitelly@gmail.com>
2022-02-09 11:22:05 +0200

    Added Cast Page

--- spa/src/Components/Cast.js
+++ spa/src/Components/Cast.js
@@ -1,11 +1,27 @@
 import React,{Component} from 'react';
+import { Container, Row, Col } from 'react-grid-system';
 
 
 export class Cast extends Component{
     render(){
         return(
             <div>
-                <h3>This is Cast</h3>
+            <center><h3>Jesse Eisenberg</h3></center>
+            {/* Movie Part */}
+            <div id='main'>
+            <Container>
+                <Row>
+                <Col sm={3}>
+                <img src={process.env.PUBLIC_URL + "/images/je.jpg"} alt="Jesse Eisenberg" class="img-thumbnail"/>
+                </Col>
+                <Col sm={3}>
+                <h3>Bio:</h3>
+                <p>Jesse Adam Eisenberg is an American actor and playwright. He made his television debut with the short-lived comedy-drama series Get Real.</p>
+                </Col>
+                </Row>
+                </Container> 
+            
+            </div>
             </div>
         )
     }
--- spa/src/Components/Movie.js
+++ spa/src/Components/Movie.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import { Container, Row, Col } from 'react-grid-system';
-
+import {Table} from 'react-bootstrap';
+import {NavLink} from "react-router-dom";
+import './Movie.css';
 
 export class Movie extends Component{
     render(){
@@ -12,18 +14,41 @@ export class Movie extends Component{
             <Container>
                 <Row>
                 <Col sm={3}>
-                <img src={process.env.PUBLIC_URL + "/images/movie1.jpg"} alt="The social network" class="img-thumbnail" class="hicon"/>
+                <img src={process.env.PUBLIC_URL + "/images/movie1.jpg"} alt="The social network" class="img-thumbnail"/>
                 </Col>
                 <Col sm={3}>
                 <h3>Plot:</h3>
                 <p>In 2003, Harvard undergrad and computer genius Mark Zuckerberg begins work on a new concept that eventually turns into the global social network known as Facebook. Six years later, he is one of the youngest billionaires ever, but Zuckerberg finds that his unprecedented success leads to both personal and legal complications when he ends up on the receiving end of two lawsuits, one involving his former friend. Based on the book "The Accidental Billionaires."</p>
                 </Col>
                 </Row>
-                </Container> 
+            </Container> 
             
             </div>
             {/* Cast Part */}
-            <div id='cast'></div>
+            <div id='cast'>
+            <center><h4>Cast</h4></center>
+            <Container>
+                <Row>
+                <Col sm={2}>
+                <img src={process.env.PUBLIC_URL + "/images/je.jpg"} alt="Jesse Eisenberg" class="img-thumbnail mcicon" />
+                </Col>
+                <Col sm={2}>
+                <NavLink to="/cast">
+                <h4>Jesse Eisenberg</h4>
+                </NavLink>
+                </Col>
+                </Row>
+                <br></br>
+                <Row>
+                <Col sm={2}>
+                <img src={process.env.PUBLIC_URL + "/images/ag.jpg"} alt="Andrew Garfield" class="img-thumbnail mcicon" />
+                </Col>
+                <Col sm={2}>
+                <h4>Andrew Garfield</h4>
+                </Col>
+                </Row>
+            </Container>
+            </div>
             </div>
         )
     }
