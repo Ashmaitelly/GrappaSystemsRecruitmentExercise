0b57425ecbc3e8ed392cbb6634862e4a5caaa797
AbdSh <ashmaitelly@gmail.com>
2022-02-12 10:19:22 +0200

    Small Change

--- spa/src/Components/WatchMovie.js
+++ spa/src/Components/WatchMovie.js
@@ -5,7 +5,6 @@ export class WatchMovie extends Component{
     constructor(props){
         super(props);
         this.url= props.location.aboutProps
-        console.log(this.url)
 
     }
     render(){
